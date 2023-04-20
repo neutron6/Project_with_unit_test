1a926d3f5d57fe53dc3d48d1818109065d9add19
rushi <rushikeshnichit@gmail.com>
2023-04-20 12:12:23 +0530

    stage 1

---
+++ project_with_unitTest/src/main/java/com/csi/project_with_unitTest/ProjectWithUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.csi.project_with_unitTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithUnitTestApplication.class, args);
+	}
+
+}
---
+++ project_with_unitTest/src/test/java/com/csi/project_with_unitTest/ProjectWithUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csi.project_with_unitTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWithUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
